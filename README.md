32a98a8eaf3406328255a93daede548620aa0793
TushariOS <tjaunjalkar@gmail.com>
2023-10-22 18:49:44 +0530

    Updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # WeatherAppSwiftUI
+## Button
+
+A `Button` in SwiftUI is used to create interactive elements that respond to user taps. You can customize the appearance and actions associated with a button.
+Example:
+Button("Tap Me") {
+    // Action to perform when the button is tapped
+}
+
+## Image
+The Image view is used to display images in SwiftUI. You can load images from your app's assets, system symbols, or network resources.
+Example:
+Image("imageName") // Load an image from your asset catalog
+Image(systemName: "star.fill") // Use a system symbol
+
+## HStack and VStack
+HStack and VStack are used to arrange views horizontally and vertically, respectively. They are the building blocks for creating flexible layouts in SwiftUI.
+Example:
+HStack {
+    Text("Left")
+    Text("Right")
+}
+
+VStack {
+    Text("Top")
+    Text("Bottom")
+}
+
+## Text
+The Text view displays text in your SwiftUI app. You can customize the font, color, and other text attributes.
+Example:
+
+Text("Hello, World!")
+    .font(.title)
+    .foregroundColor(.blue)
--- SwiftUI-WeatherApp/ContentView.swift
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -9,18 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: -10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("79°")
+                        .font(.system(size: 60, weight:  .medium))
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+                HStack(spacing: 10) {
+                    WeatherView(weekfoDay: "Mon",
+                                imageName: "cloud.snow.fill",
+                                temp: 56)
+                    WeatherView(weekfoDay: "Tue",
+                                imageName: "cloud.sun.fill",
+                                temp: 59)
+                    WeatherView(weekfoDay: "Wed",
+                                imageName: "sun.dust.fill",
+                                temp: 49)
+                    WeatherView(weekfoDay: "Thu",
+                                imageName:
+                                    "cloud.fill",
+                                temp: 63)
+                    WeatherView(weekfoDay: "FRI",
+                                imageName: "cloud.circle.fill",
+                                temp: 80)
+                    WeatherView(weekfoDay: "SAT",
+                                imageName: "cloud.heavyrain.fill",
+                                temp: 67)
+                    WeatherView(weekfoDay: "SUN",
+                                imageName: "cloud.hail.fill",
+                                temp: 81)
+                }
+                Spacer()
+                Button {
+                    print("Button Action")
+                } label: {
+                    Text("Change Day TIme")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherView: View {
+    var weekfoDay: String
+    var imageName: String
+    var temp: Int
+    
+    var body: some View {
+        VStack {
+            Text(weekfoDay)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temp)°")
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
     }
 }
